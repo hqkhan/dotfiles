bd8b1ae3d31ae31bcd7506b2f6572c5d9baeefd7
hqkhan <hqkhan2008@gmail.com>
2023-08-18 04:08:26 +0000

    misc changes

--- .config/nvim/lua/workdirs.lua
+++ .config/nvim/lua/workdirs.lua
@@ -4,8 +4,6 @@ local workdirs = {
   vim.opt.runtimepath._info.default:match("/[^,]+runtime"),
   "~/.config/nvim",
   "~/workspace",
-  "/local/home/hqkhan/workspace/json-mainline/src/ElastiCacheRedisJSONModule",
-  "/local/home/hqkhan/workspace/search-mainline/src/ElastiCacheSearch",
   "~/Sources/nvim/express_line.nvim",
   vim.fn.stdpath("data") .. "/site/pack/packer",
   vim.fn.stdpath("data") .. "/lazy",
