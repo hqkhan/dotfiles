aa37cd669195a529557e3b500396ec5f4aceadf5
hqkhan <hqkhan2008@gmail.com>
2023-08-19 04:26:45 +0000

    Added -e to echo commands to get colors

--- dots/install.sh
+++ dots/install.sh
@@ -11,21 +11,21 @@ DOT_CMD="git -c status.showUntrackedFiles=no --git-dir=${GIT_CWD}/.git -C ${HOME
 REPO_URL=$(${GIT_CMD} config --get remote.origin.url)
 
 # pull latest
-echo "[1;32mPulling[0m from [0;33m${REPO_URL}[1;32m...[0m"
+echo -e "\033[1;32mPulling\033[0m from \033[0;33m${REPO_URL}\033[1;32m...\033[0m"
 ${GIT_CMD} pull --ff-only --progress --rebase=true
 
 # reset to local HEAD
-echo "[1;32mResetting to [0;33mHEAD[1;32m...[0m"
+echo -e "\033[1;32mResetting to \033[0;33mHEAD\033[1;32m...\033[0m"
 ${DOT_CMD} reset --hard HEAD
 
 # configure repo to not display untracked files
-echo "[1;32mConfiguring repo[0m [0;33mstatus.showUntrackedFiles=no[0m"
+echo -e "\033[1;32mConfiguring repo\033[0m \033[0;33mstatus.showUntrackedFiles=no\033[0m"
 ${GIT_CMD} config --local status.showUntrackedFiles no
 
 # setup repo symbolic link at $HOME
-echo "[1;32mSetup repo link [0m [0;33m$HOME/.git[0m -> [0;33m$GIT_CWD/.git[0m"
+echo -e "\033[1;32mSetup repo link \033[0m \033[0;33m$HOME/.git\033[0m -> \033[0;33m$GIT_CWD/.git\033[0m"
 ln -fs ${GIT_CWD}/.git ${HOME}/.git
 
 # Source aliases
 alias dot="${DOT_CMD}"
-echo "[1;32mCreated the [0;33mdot[1;32m alias.[0m"
+echo -e "\033[1;32mCreated the \033[0;33mdot[1;32m alias.\033[0m"
