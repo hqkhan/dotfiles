037876e2ad79739b97cba25e73c371a6aa91fb1b
hqkhan <hqkhan2008@gmail.com>
2023-11-20 23:56:40 +0000

    hls-sens only showing counts

--- .config/nvim/lua/plugins/hls-lens.lua
+++ .config/nvim/lua/plugins/hls-lens.lua
@@ -4,7 +4,35 @@ local M = {
 }
 
 M.init = function()
-    require('hlslens').setup()
+    require('hlslens').setup({
+      override_lens = function(render, posList, nearest, idx, relIdx)
+          local sfw = vim.v.searchforward == 1
+          local indicator, text, chunks
+          local absRelIdx = math.abs(relIdx)
+          if absRelIdx > 1 then
+              indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and '' or '')
+          elseif absRelIdx == 1 then
+              indicator = sfw ~= (relIdx == 1) and '' or ''
+          else
+              indicator = ''
+          end
+
+          local lnum, col = unpack(posList[idx])
+          local cnt = #posList
+          if nearest then
+              if indicator ~= '' then
+                  text = ('[%s %d/%d]'):format(indicator, idx, cnt)
+              else
+                  text = ('[%d/%d]'):format(idx, cnt)
+              end
+              chunks = {{' ', 'Ignore'}, {text, 'HlSearchLensNear'}}
+          else
+              text = ('[%d/%d]'):format(idx, cnt)
+              chunks = {{' ', 'Ignore'}, {text, 'HlSearchLens'}}
+          end
+          render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+      end
+    })
     local kopts = {noremap = true, silent = true}
 
     vim.api.nvim_set_keymap('n', 'n',
@@ -20,5 +48,6 @@ M.init = function()
 
     -- vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
 end
+
 
 return M
