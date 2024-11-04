f1500e3a78ebfeabe32bb9e2c09ab1787576777c
hqkhan <hqkhan2008@gmail.com>
2024-11-04 16:04:41 +0000

    update highlight-undo

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -42,12 +42,25 @@ return {
     event = "VeryLazy",
     config = function()
       require('highlight-undo').setup({
-          hlgroup = 'HighlightUndo',
-          duration = 300,
-          keymaps = {
-            {'n', 'u', 'undo', {}},
-            {'n', '<C-r>', 'redo', {}},
-          }
+        duration = 300,
+        keymaps = {
+          undo = {
+            desc = "undo",
+            hlgroup = 'HighlightUndo',
+            mode = 'n',
+            lhs = 'u',
+            rhs = nil,
+            opts = {},
+          },
+          redo = {
+            desc = "redo",
+            hlgroup = 'HighlightUndo',
+            mode = 'n',
+            lhs = '<C-r>',
+            rhs = nil,
+            opts = {},
+          },
+        },
       })
     end
   },
@@ -59,16 +72,16 @@ return {
     -- Optional; default configuration will be used if setup isn't called.
     config = function()
       require("everforest").setup({
-        on_highlights = function (hl, palette)
-         hl.StatusLine = { link = "Normal" }
-         hl.Darkblue_tmux_bg =  { bg = "#081633" }
-         hl.Darkblue_tmux_fg =  { fg = "#081633" }
-         hl.CursorLine = { bg = "#100E23" }
-         hl.CursorLineNr = { fg = palette.grey1, bg = "#081633" }
-         hl.MiniIndentscopeSymbol = { fg = palette.yellow }
-         hl.GitSignsAdd = { link = "GreenSign" }
-         hl.GitSignsChange = { link = "YellowSign" }
-         hl.Visual = { link = "IncSearch" }
+        on_highlights = function(hl, palette)
+          hl.StatusLine = { link = "Normal" }
+          hl.Darkblue_tmux_bg = { bg = "#081633" }
+          hl.CursorLine = { bg = "#100E23" }
+          hl.CursorLineNr = { fg = palette.grey1, bg = "#081633" }
+          hl.MiniIndentscopeSymbol = { fg = palette.yellow }
+          hl.GitSignsAdd = { link = "GreenSign" }
+          hl.GitSignsChange = { link = "YellowSign" }
+          hl.Visual = { link = "IncSearch" }
+          hl.HighlightUndo = { fg = "#100E23", bg = palette.yellow }
         end
       })
     end,
