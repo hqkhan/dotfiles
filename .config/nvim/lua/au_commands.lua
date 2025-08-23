8e057964fb6520b9218217ebd11f1e52ed076927
hqkhan <hqkhan@dev-dsk-hqkhan-1a-f3d2f418.us-east-1.amazon.com>
2025-08-23 04:30:27 +0000

    autocmds update for nvim

--- .config/nvim/lua/au_commands.lua
+++ .config/nvim/lua/au_commands.lua
@@ -1,40 +1,84 @@
--- https://github.com/ibhagwan/nvim-lua/blob/main/lua/autocmd.lua
+local utils = require("utils")
 local aucmd = vim.api.nvim_create_autocmd
 
 local function augroup(name, fnc)
   fnc(vim.api.nvim_create_augroup(name, { clear = true }))
 end
 
-augroup("SmartTextYankPost", function(g)
-  -- highlight yanked text and copy to system clipboard
-  -- TextYankPost is also called on deletion, limit to
-  -- yanks via v:operator
-  -- if we are connected over ssh also copy using OSC52
-  aucmd("TextYankPost", {
-    group = g,
-    pattern = '*',
-    -- command = "if has('clipboard') && v:operator=='y' && len(@0)>0 | "
-    --   .. "let @+=@0 | endif | "
-    --   .. "lua vim.highlight.on_yank{higroup='IncSearch', timeout=2000}"
-    desc = "Copy to clipboard/tmux/OSC52",
-    callback = function()
-      local ok, yank_data = pcall(vim.fn.getreg, "0")
-      local valid_yank = ok and #yank_data>0 and vim.v.operator=='y'
-      if valid_yank and vim.fn.has('clipboard') == 1 then
-        pcall(vim.fn.setreg, "+", yank_data)
-      end
-      if valid_yank and vim.env.SSH_CONNECTION or
+if utils.is_root() then
+  augroup("SmartTextYankPost", function(g)
+    -- highlight yanked text and copy to system clipboard
+    -- TextYankPost is also called on deletion, limit to
+    -- yanks via v:operator
+    -- if we are connected over ssh also copy using OSC52
+    aucmd("TextYankPost", {
+      group = g,
+      pattern = "*",
+      -- command = "if has('clipboard') && v:operator=='y' && len(@0)>0 | "
+      --   .. "let @+=@0 | endif | "
+      --   .. "lua vim.highlight.on_yank{higroup='IncSearch', timeout=2000}"
+      desc = "Copy to clipboard/tmux/OSC52",
+      callback = function()
+        local ok, yank_data = pcall(vim.fn.getreg, "0")
+        local valid_yank = ok and #yank_data > 0 and vim.v.operator == "y"
+        if valid_yank and vim.fn.has("clipboard") == 1 then
+          pcall(vim.fn.setreg, "+", yank_data)
+        end
         -- $SSH_CONNECTION doesn't pass over to
         -- root when using `su -`, copy indiscriminately
-        require'utils'.is_root() then
-        require'utils'.osc52printf(yank_data)
+        if valid_yank and (vim.env.SSH_CONNECTION or utils.is_root()) then
+          utils.osc52printf(yank_data)
+        end
+        if valid_yank and vim.env.TMUX then
+          -- we use `-w` to also copy to client's clipboard
+          vim.system({ "tmux", "set-buffer", "-w", yank_data }):wait()
+        end
+        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 1000 })
       end
-      if valid_yank and vim.env.TMUX then
-        -- we use `-w` to also copy to client's clipboard
-        vim.fn.system({'tmux', 'set-buffer', '-w', yank_data})
-      end
-      vim.highlight.on_yank({ higroup='IncSearch', timeout=200 })
+    })
+  end)
+end
+
+augroup("ColorScheme", function(g)
+  aucmd("ColorSchemePre", {
+    group = g,
+    callback = function(_)
+      vim.g.fzf_colors = nil
     end
+  })
+  aucmd("ColorScheme", {
+    group = g,
+    callback = function(_)
+      -- workaround for messed up block cursor on blink.sh
+      vim.api.nvim_set_hl(0, "TermCursor", { default = false, reverse = true })
+      -- fix 'listchars' highlight on nightfly
+      if vim.g.colors_name == "nightfly" or "moonfly" then
+        vim.api.nvim_set_hl(0, "Whitespace", { default = false, link = "NonText" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { default = false, link = "LineNr" })
+        vim.api.nvim_set_hl(0, "WinSeparator", { default = false, link = "FloatBorder" })
+        vim.api.nvim_set_hl(0, "ColorColumn", { default = false, link = "PmenuSbar" })
+      elseif vim.g.colors_name:match("tokyonight") then
+        vim.api.nvim_set_hl(0, "WinSeparator", { default = false, link = "FloatBorder" })
+      end
+      -- fzf-lua
+      if type(vim.g.fzf_colors) == "table" then
+        vim.g.fzf_colors = vim.tbl_deep_extend("keep",
+          { ["bg+"] = { "bg", "Visual" } }, vim.g.fzf_colors)
+      end
+      vim.api.nvim_set_hl(0, "FzfLuaCursorLine", { default = false, link = "Visual" })
+      vim.api.nvim_set_hl(0, "FzfLuaFzfCursorLine", { default = false, link = "Visual" })
+      -- treesitter context
+      vim.api.nvim_set_hl(0, "TreesitterContext", { default = false, link = "Visual" })
+      vim.api.nvim_set_hl(0, "TreesitterContextBottom", { default = false, underline = true })
+      -- render-markdown
+      vim.api.nvim_set_hl(0, "RenderMarkdownCode", { default = false, link = "DiffChange" })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { default = false, link = "Visual" })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { default = false, link = "DiffText" })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { default = false, link = "DiffAdd" })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { default = false, link = "DiffAdd" })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { default = false, link = "DiffAdd" })
+      vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { default = false, link = "DiffAdd" })
+    end,
   })
 end)
 
@@ -54,33 +98,86 @@ augroup("MiniIndentscopeDisable", function(g)
   })
 end)
 
-augroup("ToggleSearchHL", function(g)
-  aucmd("InsertEnter",
+augroup("TermOptions", function(g)
+  aucmd("TermOpen",
     {
       group = g,
-      command = ":nohl | redraw"
+      command = "setlocal listchars= nonumber norelativenumber"
     })
 end)
 
-augroup("NewlineNoAutoComments", function(g)
-  aucmd("BufEnter", {
+augroup("ResizeWindows", function(g)
+  aucmd("VimResized",
+    {
+      group = g,
+      command = "tabdo wincmd ="
+    })
+end)
+
+augroup("ToggleSearchHL", function(g)
+  aucmd("InsertEnter", {
     group = g,
-    pattern = '*',
-    command = "setlocal formatoptions-=o"
+    callback = function()
+      vim.schedule(function() vim.cmd("nohlsearch") end)
+    end
+  })
+  aucmd("CursorMoved", {
+    group = g,
+    callback = function()
+      --[[ -- No bloat lua adpatation of: https://github.com/romainl/vim-cool
+      local view, rpos = vim.fn.winsaveview(), vim.fn.getpos(".")
+      assert(view.lnum == rpos[2])
+      assert(view.col + 1 == rpos[3])
+      -- Move the cursor to a position where (whereas in active search) pressing `n`
+      -- brings us to the original cursor position, in a forward search / that means
+      -- one column before the match, in a backward search ? we move one col forward
+      vim.cmd(string.format("silent! keepjumps go%s",
+        (vim.fn.line2byte(view.lnum) + view.col + 1 - (vim.v.searchforward == 1 and 2 or 0))))
+      -- Attempt to goto next match, if we're in an active search cursor position
+      -- should be equal to original cursor position
+      local ok, _ = pcall(vim.cmd, "silent! keepjumps norm! n")
+      local insearch = ok and (function()
+        local npos = vim.fn.getpos(".")
+        return npos[2] == rpos[2] and npos[3] == rpos[3]
+      end)()
+      -- restore original view and position
+      vim.fn.winrestview(view)
+      if not insearch then
+        vim.schedule(function() vim.cmd("nohlsearch") end)
+      end ]]
+      if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+        vim.schedule(function() vim.cmd.nohlsearch() end)
+      end
+    end
   })
 end)
 
 augroup("ActiveWinCursorLine", function(g)
   -- Highlight current line only on focused window
-  aucmd({ "WinEnter", "BufEnter", "InsertLeave" }, {
+  local callback = function()
+    local curwin = vim.api.nvim_get_current_win()
+    for _, w in ipairs(vim.api.nvim_list_wins()) do
+      vim.wo[w].cursorline = w == curwin
+    end
+  end
+  aucmd({ "WinEnter", "BufEnter", "InsertLeave" }, { group = g, callback = callback })
+  aucmd({ "WinLeave", "BufLeave", "InsertEnter" }, { group = g, callback = callback })
+end)
+
+-- goto last location when opening a buffer
+augroup("BufLastLocation", function(g)
+  aucmd("BufReadPost", {
     group = g,
-    pattern = '*',
-    command = 'if ! &cursorline && ! &pvw | setlocal cursorline | endif'
-  })
-  aucmd({ "WinLeave", "BufLeave", "InsertEnter" }, {
-    group = g,
-    pattern = '*',
-    command = 'if &cursorline && ! &pvw | setlocal nocursorline | endif'
+    callback = function(e)
+      -- skip fugitive commit message buffers
+      local bufname = vim.api.nvim_buf_get_name(e.buf)
+      if bufname:match("COMMIT_EDITMSG$") then return end
+      local mark = vim.api.nvim_buf_get_mark(e.buf, '"')
+      local line_count = vim.api.nvim_buf_line_count(e.buf)
+      if mark[1] > 0 and mark[1] <= line_count then
+        vim.cmd 'normal! g`"zz'
+      end
+    end,
   })
 end)
 
@@ -88,8 +185,17 @@ end)
 augroup("Fugitive", function(g)
   aucmd("BufReadPost", {
     group = g,
-    pattern = 'fugitive://*',
-    command = 'set bufhidden=delete'
+    pattern = "fugitive:*",
+    command = "set bufhidden=delete"
+  })
+end)
+
+-- Solidity abi JSON
+augroup("SolidityABI", function(g)
+  aucmd({ "BufRead", "BufNewFile" }, {
+    group = g,
+    pattern = "*.abi",
+    command = "set filetype=jsonc"
   })
 end)
 
@@ -148,14 +254,6 @@ end)
 
 -- https://vim.fandom.com/wiki/Avoid_scrolling_when_switch_buffers
 augroup("DoNotAutoScroll", function(g)
-  local function is_float(winnr)
-    local wincfg = vim.api.nvim_win_get_config(winnr)
-    if wincfg and (wincfg.external or wincfg.relative and #wincfg.relative > 0) then
-      return true
-    end
-    return false
-  end
-
   aucmd("BufLeave", {
     group = g,
     desc = "Avoid autoscroll when switching buffers",
@@ -166,7 +264,7 @@ augroup("DoNotAutoScroll", function(g)
       local from_buf = vim.api.nvim_get_current_buf()
       local from_win = vim.fn.bufwinid(from_buf)
       local to_win = vim.api.nvim_get_current_win()
-      if not is_float(to_win) and not is_float(from_win) then
+      if not utils.win_is_float(to_win) and not utils.win_is_float(from_win) then
         vim.b.__VIEWSTATE = vim.fn.winsaveview()
       end
     end
@@ -177,7 +275,7 @@ augroup("DoNotAutoScroll", function(g)
     callback = function()
       if vim.b.__VIEWSTATE then
         local to_win = vim.api.nvim_get_current_win()
-        if not is_float(to_win) then
+        if not utils.win_is_float(to_win) then
           vim.fn.winrestview(vim.b.__VIEWSTATE)
         end
         vim.b.__VIEWSTATE = nil
@@ -185,40 +283,6 @@ augroup("DoNotAutoScroll", function(g)
     end
   })
 end)
-
--- goto last location when opening a buffer
-augroup("BufLastLocation", function(g)
-  aucmd("BufReadPost", {
-    group = g,
-    callback = function(e)
-      -- skip fugitive commit message buffers
-      local bufname = vim.api.nvim_buf_get_name(e.buf)
-      if bufname:match("COMMIT_EDITMSG$") then return end
-      local mark = vim.api.nvim_buf_get_mark(e.buf, '"')
-      local line_count = vim.api.nvim_buf_line_count(e.buf)
-      if mark[1] > 0 and mark[1] <= line_count then
-        vim.cmd 'normal! g`"zz'
-      end
-    end,
-  })
-end)
-
-vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
-  command = "if mode() != 'c' | checktime | endif",
-  pattern = { "*" },
-})
-
-vim.api.nvim_create_autocmd({ "BufRead" }, {
-  pattern = { "gitmux.conf" },
-  callback = function()
-    vim.cmd([[set filetype=sh]])
-  end,
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "*tmux.conf" },
-  command = "execute 'silent !tmux source <afile> --silent'",
-})
 
 augroup("GQFormatter", function(g)
   aucmd({ "FileType", "LspAttach" },
@@ -232,6 +296,16 @@ augroup("GQFormatter", function(g)
           return
         end
         require("plugins.conform")._set_gq_keymap(e)
+      end,
+    })
+end)
+
+augroup("LspAttach", function(g)
+  aucmd({ "LspAttach" },
+    {
+      group = g,
+      callback = function(_)
+        require("lsp.keymaps").setup()
       end,
     })
 end)
